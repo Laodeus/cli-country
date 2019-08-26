654ec7cbfa4a4225d2feb84594d0a44d22bc7204
Jonathan <atta.jon.t@gmail.com>
2019-08-26 14:44:20 +0200

    finished

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ cli-country
 to install => 
     npm i
 to launch
-    "cli-holiday [arg] [arg] [arg]...."
+    "cli-holiday [arg] [arg] ...[year optional]...[arg]"
 
     [arg] == a country or a country code on 2 digit.    
-    
+    [year] == a year in format 4 digit. If not set, default year is the curent year.
--- index.js
+++ index.js
@@ -4,26 +4,57 @@ console.clear();
 console.log("So... It begin... brace yourself");
 
 const { getCode, getName } = require('country-list');
+const axios = require('axios');
 
-let country2digit = []
+let country2digit = [];
+let year = new Date().getFullYear();
+
+const getHolliday = async (country, year) => {
+    try {
+        uri = "https://date.nager.at/api/v2/publicholidays";
+        const response = await axios.get(`${uri}/${year}/${country}`);
+        return response;
+    }
+    catch (error) {
+        console.log("Sometimes, things would seems to fall appart... => " + error);
+    }
+};
+
 
 
 process.argv.forEach((element, id) => {
 
     if (getCode(element) != undefined && id > 1) {
         country2digit[country2digit.length] = getCode(element);
-        console.log(`id => ${id} => ${element} => ${getCode(element)} `);
+        //console.log(`id => ${id} => ${element} => ${getCode(element)} `);
 
     }
     else if (getName(element) != undefined && id > 1) {
         country2digit[country2digit.length] = element;
-        console.log(`id => ${id} => ${element} => ${getName(element)} `);
+        //console.log(`id => ${id} => ${element} => ${getName(element)} `);
 
     }
+    else if (/^[0-9][0-9][0-9][0-9]$/.test(element) && id > 1) {
+        year = element;
+        //console.log(`the date ${element} will be use for processing. `);
 
 
+    }
     else {
-        id > 1 ? console.log(`id => ${id} => ${element} => is not a valid country! you bastard!!!!!!!!!!!`) : "";
+        // id > 1 ? console.log(`id => ${id} => ${element} => is not a valid country! `) :"" ;
     }
 
-}); 
+});
+
+console.clear();
+console.log(`${year} will be used for the request.`);
+
+
+
+country2digit.forEach(async (elem) => {
+    const response = await getHolliday(elem, year);
+    console.log(`${elem} => `);
+    response.data.forEach((respElem,id)=>{
+        console.log(`   element ${id} => ${respElem.localName} is ${respElem.date}`)
+    });
+});
